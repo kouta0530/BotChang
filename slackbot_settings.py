f686133ab64b6c01298a6ec1ff8af0c4c17d5645
kouta0530 <16jk047@ymklab.net>
2020-08-11 21:23:44 +0900

    test

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -2,10 +2,12 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
+"""
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path,encoding="utf-8_sig")
+"""
 
-API_TOKEN = os.environ['API_TOKEN']
+API_TOKEN = os.environ['SLACKBOT_API_TOKEN']
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "I dont't understand you."
 # プラグインスクリプトを置いてあるサブディレクトリ名のリスト
