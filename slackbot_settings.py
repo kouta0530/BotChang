2db959ed22f13700b5553009454bf9d7f4607c7a
kouta0530 <16jk047@ymklab.net>
2020-08-11 17:38:01 +0900

    環境変数関連の修正

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -2,11 +2,12 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
+"""
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path,encoding="utf-8_sig")
+"""
 
 TOKEN = os.environ.get("API_TOKEN")
-
 
 API_TOKEN = TOKEN
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
