c9d37394c34eaf5fb8f56e73bbeb681f606042a4
kouta0530 <16jk047@ymklab.net>
2020-08-11 17:58:39 +0900

    os.environの修正

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -7,9 +7,7 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path,encoding="utf-8_sig")
 """
 
-TOKEN = os.environ.get("API_TOKEN")
-
-API_TOKEN = TOKEN
+API_TOKEN = os.environ["API_TOKEN"]
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = "I dont't understand you."
 
