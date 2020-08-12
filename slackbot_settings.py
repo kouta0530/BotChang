98cdd4e78e0d70fa654cfb419f3c970432e80c73
kouta0530 <16jk047@ymklab.net>
2020-08-13 02:29:42 +0900

    test

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -11,4 +11,4 @@ API_TOKEN = os.environ.get('SLACKBOT_API_TOKEN')
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = os.environ.get('SLACKBOT_DEFAULT_REPLY')
 # プラグインスクリプトを置いてあるサブディレクトリ名のリスト
-PLUGINS = os.environ.get('SLACKBOT_PLGUINS')
+PLUGINS = os.environ.get('SLACKBOT_PLUGINS')
