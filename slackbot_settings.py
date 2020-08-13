a5373fbc2f92da5d45e6e2ef14ce4f9faf639f30
kouta0530 <16jk047@ymklab.net>
2020-08-13 14:30:18 +0900

    test

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -2,13 +2,12 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-"""
+
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path,encoding="utf-8_sig")
-"""
 
 API_TOKEN = os.environ.get('SLACKBOT_API_TOKEN')
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
 DEFAULT_REPLY = os.environ.get('SLACKBOT_DEFAULT_REPLY')
 # プラグインスクリプトを置いてあるサブディレクトリ名のリスト
-PLUGINS = ["plugin"]#os.environ.get('SLACKBOT_PLUGINS').split()
+PLUGINS = ["plugins"]#os.environ.get('SLACKBOT_PLUGINS').split()
