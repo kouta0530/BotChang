c1b6a3487f305a4875013a986e2f004e9b574f9a
kouta0530 <16jk047@ymklab.net>
2020-08-13 11:46:06 +0900

    test

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -2,10 +2,10 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-
+"""
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path,encoding="utf-8_sig")
-
+"""
 
 API_TOKEN = os.environ.get('SLACKBOT_API_TOKEN')
 # このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
