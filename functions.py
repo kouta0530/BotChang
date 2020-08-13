d78441709e4ca38b790b983bb10542fc14164c4e
kouta0530 <16jk047@ymklab.net>
2020-08-13 17:24:57 +0900

    test

--- functions.py
+++ functions.py
@@ -7,8 +7,10 @@ def get_news():
 
     content = soup.find("div",{"class":"colBoxOuter"})
     news = content.find("div",{"class":"colBoxTitle"}).find("h3").find("a").get("href")
+    
     """
     news = [t.find("h3").find("a").get("href") for t in content.find_all("div",{"class":"colBoxTitle"})]
     news = [soup.find("h3").find("a").get("href")]
     """
+
     return news
